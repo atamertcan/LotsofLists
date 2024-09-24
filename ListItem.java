63d4403f49b25e8071dccd22d3da87c04d0e1efd
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-24 17:06:30 +0300

    ListItem.java

--- ListItem.java
+++ ListItem.java
@@ -1,1 +1,10 @@
-
+public abstract class ListItem {
+    
+    protected ListItem rightLink;
+    protected ListItem leftLink;
+    protected Object value;
+    
+    public ListItem(Object value){
+        
+        this.value = value;
+    }
