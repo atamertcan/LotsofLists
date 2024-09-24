338e1014b7a4b8af4f26f9776eeeaf7d3fb66a90
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-24 17:09:58 +0300

    SearchTree.java

--- SearchTree.java
+++ SearchTree.java
@@ -10,3 +10,31 @@ public class SearchTree implements NodeList{
     public ListItem getRoot(){
         return root;
     }
+
+    @Override
+    public boolean addItem(ListItem node){
+        
+        if (root == null){
+            root = node;
+            return true;
+        }
+        
+        ListItem current = root;
+        while(true){
+            
+            if(current.next() == null){
+                
+                current.setNext(node);
+                return true;
+            }
+            
+            if(current.compareTo(node) == 1){
+                
+                current.setPrevious(node);
+                return true;
+            }
+            
+            current = current.next();
+            return false;
+        }
+    }
