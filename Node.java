a2b673f9190b8e40c0696c9ac74209feae3f20dd
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-24 17:07:20 +0300

    Node.java

--- Node.java
+++ Node.java
@@ -1,1 +1,10 @@
-
+public class Node extends ListItem{
+    
+    public Node(Object value){
+        super(value);
+    }
+    
+    @Override
+    ListItem next(){
+        return rightLink;
+    }
