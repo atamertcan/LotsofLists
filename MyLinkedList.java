034ce124dfc05a0058fb139ab1fcd980067f4d95
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-24 17:08:35 +0300

    MyLinkedList.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -10,3 +10,32 @@ public class MyLinkedList implements NodeList{
     public ListItem getRoot(){
         return root;
     }
+
+    @Override
+    public boolean addItem(ListItem node){
+        
+        if (root == null){
+            root = node;
+            return true;
+        }
+        
+        ListItem current = root;
+        while(true){
+            
+            if(current.next() == null){
+                
+                current.setNext(node);
+                node.setPrevious(current);
+                return true;
+            }
+            
+            if(current.compareTo(node) == 1){
+                
+                root = node;
+                root.setNext(current);
+                current.setPrevious(root);
+                return true;
+            }
+            return false;
+        }
+    }
