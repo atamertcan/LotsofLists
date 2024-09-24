011adf0c0d1670b121efcd60ab4f6cc7ff06cd69
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-24 17:09:43 +0300

    SearchTree.java

--- SearchTree.java
+++ SearchTree.java
@@ -1,1 +1,12 @@
-
+public class SearchTree implements NodeList{
+    
+    private ListItem root;
+    
+    public SearchTree(ListItem root){
+        this.root = root;
+    }
+    
+    @Override
+    public ListItem getRoot(){
+        return root;
+    }
