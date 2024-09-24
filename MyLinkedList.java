f45d07d0636b659252f72bb7635f7d685f092fae
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-24 17:08:18 +0300

    MyLinkedList.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,1 +1,12 @@
-
+public class MyLinkedList implements NodeList{
+    
+    private ListItem root;
+    
+    public MyLinkedList(ListItem root){
+        this.root = root;
+    }
+    
+    @Override
+    public ListItem getRoot(){
+        return root;
+    }
