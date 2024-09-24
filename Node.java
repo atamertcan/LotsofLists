287a47bf75627cbc9c5eff8f05fb4212b960da4f
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-24 17:07:49 +0300

    Node.java

--- Node.java
+++ Node.java
@@ -18,3 +18,26 @@ public class Node extends ListItem{
     ListItem previous(){
         return leftLink;
     }
+
+    @Override
+    ListItem setPrevious(ListItem leftLink){
+        return this.leftLink = leftLink;
+    }
+    
+    @Override
+    int compareTo(ListItem other){
+        
+        int valueInt = Integer.parseInt(String.valueOf(value));
+        int otherInt = Integer.parseInt(String.valueOf(other.value));
+        
+        if(valueInt > otherInt){
+            return 1;
+        }
+        else if (otherInt > valueInt){
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
